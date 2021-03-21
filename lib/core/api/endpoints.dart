c50f81c2b58c070bf62d225f77d5fa58b9189a86
Rodrigo Amancio <rodrigoa.work@gmail.com>
2021-03-21 12:36:59 -0300

    create endpoint, api interceptor and another classes to usage in app

---
+++ lib/core/api/endpoints.dart
@@ -0,0 +1,5 @@
+class Endpoints {
+  static const user = 'users';
+  static const createUser = 'users/MyNewUserId';
+  static const currentUser = 'users/me';
+}
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,8 @@
 import 'package:appanime/features/home_screen/presentation/bloc/home_screen_bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:get_it/get_it.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:http/http.dart' as http;
 
 final sl = GetIt.instance;
 
@@ -8,5 +10,15 @@ Future<void> init() async {
   sl.registerLazySingleton(() => HomeScreenBloc());
 
   //! External dependecies
-  await Firebase.initializeApp();
+  await Parse().initialize(
+    'MkWBuU8WugU3s66px38qEidt5s1RX95PyALR46Jp',
+    'https://parseapi.back4app.com/',
+    clientKey: 'U990oEgp8K7esY0Vm6JYU3JFVCjHDcUhZiweEkbx',
+    autoSendSessionId: true,
+    debug: true,
+  );
+
+  sl.registerLazySingleton(() => http.Client);
+  sl.registerLazySingleton(() => Connectivity());
+  await sl.allReady();
 }
